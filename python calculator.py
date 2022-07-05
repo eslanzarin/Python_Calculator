169b060b9cb15e2e1197eac2325f2f01e84833c6
eslanzarin <eslanzarin@gmail.com>
2022-07-05 19:45:52 -0300

    calculator done, without comments yet

--- python calculator.py
+++ python calculator.py
@@ -1,5 +1,23 @@
 from tkinter import*
 
+from setuptools import Command
+
+def btnClick(numbers):
+    global operator
+    operator=operator + str(numbers)
+    text_Input.set(operator)
+
+def btnClearDisplay():
+    global operator
+    operator=""
+    text_Input.set("")
+
+def btnEqualsInput():
+    global operator
+    sumup=str(eval(operator))
+    text_Input.set(sumup)
+    operator=""
+
 cal = Tk()
 cal.title("Calculator")
 operator=""
@@ -9,42 +27,42 @@ txtDisplay = Entry(cal, font=('arial', 20, 'bold'), textvariable=text_Input, bd=
 
 # FIRST ROW
 
-btn7=Button (cal, padx=16,bd=8, fg="black", font=('arial', 20, 'bold'), text="7", bg="powder blue").grid(row=1, column=0)
+btn7=Button (cal, padx=16, pady = 16, bd=8, fg="black", font=('arial', 20, 'bold'), text="7", bg="powder blue", command=lambda:btnClick(7)).grid(row=1, column=0)
 
-btn8=Button (cal, padx=16,bd=8, fg="black", font=('arial', 20, 'bold'), text="8", bg="powder blue").grid(row=1, column=1)
+btn8=Button (cal, padx=16, pady = 16,bd=8, fg="black", font=('arial', 20, 'bold'), text="8", bg="powder blue", command=lambda:btnClick(8)).grid(row=1, column=1)
 
-btn9=Button (cal, padx=16,bd=8, fg="black", font=('arial', 20, 'bold'), text="9", bg="powder blue").grid(row=1, column=2)
+btn9=Button (cal, padx=16, pady = 16,bd=8, fg="black", font=('arial', 20, 'bold'), text="9", bg="powder blue", command=lambda:btnClick(9)).grid(row=1, column=2)
 
-Addition=Button (cal, padx=16,bd=8, fg="black", font=('arial', 20, 'bold'), text="+", bg="powder blue").grid(row=1, column=3)
+Addition=Button (cal, padx=16, pady = 16,bd=8, fg="black", font=('arial', 20, 'bold'), text="+", bg="powder blue", command=lambda:btnClick("+")).grid(row=1, column=3)
 
 # SECOND ROW
 
-btn4=Button (cal, padx=16,bd=8, fg="black", font=('arial', 20, 'bold'), text="4", bg="powder blue").grid(row=2, column=0)
+btn4=Button (cal, padx=16, pady = 16,bd=8, fg="black", font=('arial', 20, 'bold'), text="4", bg="powder blue", command=lambda:btnClick(4)).grid(row=2, column=0)
 
-btn5=Button (cal, padx=16,bd=8, fg="black", font=('arial', 20, 'bold'), text="5", bg="powder blue").grid(row=2, column=1)
+btn5=Button (cal, padx=16, pady = 16,bd=8, fg="black", font=('arial', 20, 'bold'), text="5", bg="powder blue", command=lambda:btnClick(5)).grid(row=2, column=1)
 
-btn6=Button (cal, padx=16,bd=8, fg="black", font=('arial', 20, 'bold'), text="6", bg="powder blue").grid(row=2, column=2)
+btn6=Button (cal, padx=16, pady = 16,bd=8, fg="black", font=('arial', 20, 'bold'), text="6", bg="powder blue", command=lambda:btnClick(6)).grid(row=2, column=2)
 
-Subtraction=Button (cal, padx=16,bd=8, fg="black", font=('arial', 20, 'bold'), text="-", bg="powder blue").grid(row=2, column=3)
+Subtraction=Button (cal, padx=16, pady = 16,bd=8, fg="black", font=('arial', 20, 'bold'), text="-", bg="powder blue", command=lambda:btnClick("-")).grid(row=2, column=3)
 
 # THIRD ROW
 
-btn1=Button (cal, padx=16,bd=8, fg="black", font=('arial', 20, 'bold'), text="1", bg="powder blue").grid(row=3, column=0)
+btn1=Button (cal, padx=16, pady = 16,bd=8, fg="black", font=('arial', 20, 'bold'), text="1", bg="powder blue", command=lambda:btnClick(1)).grid(row=3, column=0)
 
-btn2=Button (cal, padx=16,bd=8, fg="black", font=('arial', 20, 'bold'), text="2", bg="powder blue").grid(row=3, column=1)
+btn2=Button (cal, padx=16, pady = 16,bd=8, fg="black", font=('arial', 20, 'bold'), text="2", bg="powder blue", command=lambda:btnClick(2)).grid(row=3, column=1)
 
-btn3=Button (cal, padx=16,bd=8, fg="black", font=('arial', 20, 'bold'), text="3", bg="powder blue").grid(row=3, column=2)
+btn3=Button (cal, padx=16, pady = 16,bd=8, fg="black", font=('arial', 20, 'bold'), text="3", bg="powder blue", command=lambda:btnClick(3)).grid(row=3, column=2)
 
-Multiplication=Button (cal, padx=16,bd=8, fg="black", font=('arial', 20, 'bold'), text="*", bg="powder blue").grid(row=3, column=3)
+Multiplication=Button (cal, padx=16, pady = 16,bd=8, fg="black", font=('arial', 20, 'bold'), text="*", bg="powder blue", command=lambda:btnClick("*")).grid(row=3, column=3)
 
 #FOURTH ROW
 
-btn0=Button (cal, padx=16,bd=8, fg="black", font=('arial', 20, 'bold'), text="0", bg="powder blue").grid(row=4, column=0)
+btn0=Button (cal, padx=16, pady = 16,bd=8, fg="black", font=('arial', 20, 'bold'),text="0", bg="powder blue", command=lambda:btnClick(0)).grid(row=4, column=0)
 
-btnClear=Button (cal, padx=16,bd=8, fg="black", font=('arial', 20, 'bold'), text="C", bg="powder blue").grid(row=4, column=1)
+btnClear=Button (cal, padx=16, pady = 16,bd=8, fg="black", font=('arial', 20, 'bold'), text="C", bg="powder blue", command=btnClearDisplay).grid(row=4, column=1)
 
-btnEquals=Button (cal, padx=16,bd=8, fg="black", font=('arial', 20, 'bold'), text="=", bg="powder blue").grid(row=4, column=2)
+btnEquals=Button (cal, padx=16, pady = 16,bd=8, fg="black", font=('arial', 20, 'bold'), text="=", bg="powder blue", command=btnEqualsInput).grid(row=4, column=2)
 
-Division=Button (cal, padx=16,bd=8, fg="black", font=('arial', 20, 'bold'), text="/", bg="powder blue").grid(row=4, column=3)
+Division=Button (cal, padx=16, pady = 16,bd=8, fg="black", font=('arial', 20, 'bold'), text="/", bg="powder blue", command=lambda:btnClick("/")).grid(row=4, column=3)
 
 cal.mainloop()

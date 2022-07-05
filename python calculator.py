35342c1bf49ec5dd15b1587c1100f42f9471ddcb
eslanzarin <eslanzarin@gmail.com>
2022-07-05 10:51:36 -0300

    Tkinter buttons

--- python calculator.py
+++ python calculator.py
@@ -7,7 +7,38 @@ text_Input = StringVar()
 
 txtDisplay = Entry(cal, font=('arial', 20, 'bold'), textvariable=text_Input, bd=30, insertwidth=4, bg="powder blue", justify = 'right').grid(columnspan=4)
 
-btn7=Button (cal, padx=16,bd=8, fg="black", font=('arial', 20, 'bold'), text="7").grid(row=1, column=0)
+# FIRST ROW
+
+btn7=Button (cal, padx=16,bd=8, fg="black", font=('arial', 20, 'bold'), text="7", bg="powder blue").grid(row=1, column=0)
+
+btn8=Button (cal, padx=16,bd=8, fg="black", font=('arial', 20, 'bold'), text="8", bg="powder blue").grid(row=1, column=1)
+
+btn9=Button (cal, padx=16,bd=8, fg="black", font=('arial', 20, 'bold'), text="9", bg="powder blue").grid(row=1, column=2)
+
+Addition=Button (cal, padx=16,bd=8, fg="black", font=('arial', 20, 'bold'), text="+", bg="powder blue").grid(row=1, column=3)
+
+# SECOND ROW
+
+btn4=Button (cal, padx=16,bd=8, fg="black", font=('arial', 20, 'bold'), text="4", bg="powder blue").grid(row=2, column=0)
+
+btn5=Button (cal, padx=16,bd=8, fg="black", font=('arial', 20, 'bold'), text="5", bg="powder blue").grid(row=2, column=1)
+
+btn6=Button (cal, padx=16,bd=8, fg="black", font=('arial', 20, 'bold'), text="6", bg="powder blue").grid(row=2, column=2)
+
+Subtraction=Button (cal, padx=16,bd=8, fg="black", font=('arial', 20, 'bold'), text="-", bg="powder blue").grid(row=2, column=3)
+
+# THIRD ROW
+
+btn1=Button (cal, padx=16,bd=8, fg="black", font=('arial', 20, 'bold'), text="1", bg="powder blue").grid(row=3, column=0)
+
+btn2=Button (cal, padx=16,bd=8, fg="black", font=('arial', 20, 'bold'), text="2", bg="powder blue").grid(row=3, column=1)
+
+btn3=Button (cal, padx=16,bd=8, fg="black", font=('arial', 20, 'bold'), text="3", bg="powder blue").grid(row=3, column=2)
+
+Multiplication=Button (cal, padx=16,bd=8, fg="black", font=('arial', 20, 'bold'), text="*", bg="powder blue").grid(row=3, column=3)
+
+#FOURTH ROW
+
 
 cal.mainloop()
 
